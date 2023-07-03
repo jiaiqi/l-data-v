3b907d96f1cba560294d2eafeb0252d0cd10a81a
jiaqi <2468260248@qq.com>
2023-07-03 11:38:39 +0800

    支持條件篩選

--- src/views/request-preview.vue
+++ src/views/request-preview.vue
@@ -1,36 +1,71 @@
 <template>
   <div class="page-wrap">
-    <!-- 标题 -->
-    <div class="title" v-if="config && config.srv_req_name">
-      {{ config.srv_req_name }}
-    </div>
-    <!-- 筛选项 -->
-    <div class="group-box" v-if="groupByCols">
-      <div class="group-box-item" v-for="(groupItem, key) in groupByCols">
-        <el-radio-group @input="changeGroup($event, groupItem.list, key)" v-model="groupItem.value">
-          <el-radio :label="index" :value="item.col_name" v-for="(item, index) in  groupItem.list"
-            @click.native="clickRadio($event, key, index)">
-            <span>{{ item.label }}</span>
-          </el-radio>
-        </el-radio-group>
-      </div>
-    </div>
-    <!-- 过滤条件 -->
-    <div></div>
 
-    <!-- 表格 -->
-    <el-table :data="tableData" border stripe style="width: 100%" :span-method="objectSpanMethod" v-loading="onLoading"
-      :header-cell-style="{ 'background': '#f0f3f9', 'font-weight': 'bold', 'color': '#000' }">
-      <el-table-column :prop="column.columns" :label="column.label" min-width="180" v-for="column in srvCols">
-      </el-table-column>
-    </el-table>
+    <el-container>
+      <el-header>
+        <div class="title" v-if="config">
+          {{ config.srv_req_name || config.list_name || '' }}
+        </div>
+      </el-header>
+      <el-main>
+        <!-- 分组 -->
+        <div class="group-box" v-if="groupByCols">
+          <div class="group-box-item" v-for="(groupItem, key) in groupByCols">
+            <el-radio-group @input="changeGroup($event, groupItem.list, key)" v-model="groupItem.value">
+              <el-radio :label="index" :value="item.col_name" v-for="(item, index) in groupItem.list"
+                @click.native="clickRadio($event, key, index)">
+                <span>{{ item.label }}</span>
+              </el-radio>
+            </el-radio-group>
+          </div>
+        </div>
+        <!-- 筛选 -->
+        <div v-if="filterCols && filterCols.length > 0">
+          <el-form ref="form" :model="filterModel" label-width="120px">
+            <el-row>
+              <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="4" v-for="item in filterCols">
+                <el-form-item :label="item.label">
+                  <el-date-picker value-format="yyyy-MM-dd" v-model="item.value" type="daterange" range-separator="至"
+                    start-placeholder="开始日期" end-placeholder="结束日期" v-if="item.col_type === 'Date'"
+                    @change="valueChange($event, item)">
+                  </el-date-picker>
+                  <el-input v-model="item.value" clearable v-else-if="item.col_type === 'String'"
+                    @change="valueChange($event, item)"></el-input>
+                  <!-- <el-input v-model.number="item.value" type="number"
+                    v-else-if="['Money', 'Float', 'Int', 'Integer'].includes(item.col_type)"
+                    @change="valueChange($event, item)"></el-input> -->
+                  <el-input-number v-model="item.value" clearable label="描述文字"
+                    v-else-if="['Money', 'Float'].includes(item.col_type)"
+                    @change="valueChange($event, item)"></el-input-number>
+                  <el-input-number v-model="item.value" clearable v-else-if="['Int', 'Integer'].includes(item.col_type)"
+                    @change="valueChange($event, item)"></el-input-number>
+                </el-form-item>
+              </el-col>
+              <!-- <el-col :xs="8" :sm="6" :md="4" :lg="2" :xl="1" align="right">
+                <el-button type="primary" @click="resetFilter">重置</el-button>
+              </el-col> -->
+            </el-row>
+          </el-form>
+        </div>
 
-    <div v-if="page" style="padding: 20px;text-align: center;">
-      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page.pageNo"
-        :page-sizes="[10, 20, 50, 100]" :page-size="page.pageSize" layout="total, sizes, prev, pager, next, jumper"
-        :total="page.total">
-      </el-pagination>
-    </div>
+        <!-- 数据 -->
+        <el-table :data="tableData" border stripe style="width: 100%" :span-method="objectSpanMethod"
+          v-loading="onLoading" :header-cell-style="{
+            background: '#f0f3f9',
+            'font-weight': 'bold',
+            color: '#000',
+          }">
+          <el-table-column :prop="column.columns" :label="column.label" min-width="180" v-for="column in srvCols">
+          </el-table-column>
+        </el-table>
+      </el-main>
+      <el-footer v-if="page" style="padding: 20px; text-align: center">
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page.pageNo"
+          :page-sizes="[10, 20, 50, 100]" :page-size="page.pageSize" layout="total, sizes, prev, pager, next, jumper"
+          :total="page.total">
+        </el-pagination>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -46,14 +81,23 @@ export default {
       page: null,
       groupCols: [],
       calcCols: [],
-      groupByCols: {},// 分组字段 可能重复
+      groupByCols: {}, // 分组字段 可能重复
       groupByColsVal: {},
       onLoading: false,
       curGroup: null,
       current: "",
+      filterCols: [],
     };
   },
   computed: {
+    filterModel() {
+      if (Array.isArray(this.filterCols) && this.filterCols.length > 0) {
+        return this.filterCols.reduce((res, cur) => {
+          res[cur.columns] = cur.value
+          return res
+        }, {})
+      }
+    },
     requestNo() {
       return this.$route.query?.requestNo;
     },
@@ -62,54 +106,70 @@ export default {
     },
   },
   methods: {
+    resetFilter() {
+      this.filterCols = this.filterCols.map(item => {
+        item.value = undefined
+        return item
+      })
+    },
+    valueChange(e, columnInfo) {
+      console.log(e, columnInfo);
+      console.log(this.filterModel)
+      this.getList()
+    },
     clickRadio(e, key, index) {
       console.log(key, index);
       if (this.groupByCols[key].value === index) {
         e.preventDefault();
 
-        this.groupByCols[key].value = -1
-        this.$set(this.groupByCols[key], 'value', -1)
-        this.curGroup = this.curGroup.filter(item => item.colName !== key)
-        if (Object.keys(this.groupByCols).every(key => (typeof this.groupByCols[key]?.value === 'number' && this.groupByCols[key]?.value > -1)||this.groupByCols[key]?.value !== 'number')) {
-          this.curGroup = []
+        this.groupByCols[key].value = -1;
+        this.$set(this.groupByCols[key], "value", -1);
+        this.curGroup = this.curGroup.filter((item) => item.colName !== key);
+        if (
+          Object.keys(this.groupByCols).every(
+            (key) =>
+              (typeof this.groupByCols[key]?.value === "number" &&
+                this.groupByCols[key]?.value > -1) ||
+              this.groupByCols[key]?.value !== "number"
+          )
+        ) {
+          this.curGroup = [];
         }
-        this.getList()
+        this.getList();
       }
-
     },
     changeGroup(val, cols) {
-      if (typeof val === 'number' && Array.isArray(cols) && cols.length > val) {
-        const info = cols[val]
-        console.log(this.groupByColsVal)
-        const groupList = []
+      if (typeof val === "number" && Array.isArray(cols) && cols.length > val) {
+        const info = cols[val];
+        console.log(this.groupByColsVal);
+        const groupList = [];
         for (const key in this.groupByCols) {
           if (Object.hasOwnProperty.call(this.groupByCols, key)) {
             const item = this.groupByCols[key].list;
-            if (typeof this.groupByCols[key].value === 'number' && this.groupByCols[key].value > -1) {
-              const info = item[this.groupByCols[key].value]
+            if (
+              typeof this.groupByCols[key].value === "number" &&
+              this.groupByCols[key].value > -1
+            ) {
+              const info = item[this.groupByCols[key].value];
               if (info.row_json) {
                 try {
-                  let group = JSON.parse(info.row_json)
-                  groupList.push(group)
-
-                } catch (error) {
-
-                }
+                  let group = JSON.parse(info.row_json);
+                  groupList.push(group);
+                } catch (error) { }
               }
             }
           }
         }
-        this.curGroup = [...groupList, ...this.calcCols]
+        this.curGroup = [...groupList, ...this.calcCols];
         // this.getList({ group: [group, ...this.groupCols, ...this.calcCols] })
-        this.getList()
-        return
+        this.getList();
+        return;
       }
-
     },
     objectSpanMethod({ row, column, rowIndex, columnIndex }) {
       const colName = column.property;
       // 只有group type是by的字段可以合并
-      if (this.groupCols.find(item => item.colName === colName)) {
+      if (this.groupCols.find((item) => item.colName === colName)) {
         const currentValue = row[colName];
         let rowspan = 1;
         // 遍历之后的行，判断是否需要合并
@@ -134,17 +194,62 @@ export default {
         // 返回合并的行数和列数
         return {
           rowspan,
-          colspan: 1
-        }
+          colspan: 1,
+        };
       }
     },
     handleSizeChange(val) {
-      this.page.pageSize = val
-      this.getList()
+      this.page.pageSize = val;
+      this.getList();
     },
     handleCurrentChange(val) {
-      this.page.pageNo = val
-      this.getList()
+      this.page.pageNo = val;
+      this.getList();
+    },
+    getListCfg() {
+      const url = "/config/select/srvpage_cfg_com_list_select";
+      const req = {
+        serviceName: "srvpage_cfg_com_list_select",
+        colNames: ["*"],
+        condition: [{
+          colName: 'list_no',
+          ruleType: 'eq',
+          value: this.requestNo
+        }],
+        page: { pageNo: 1, rownumber: 1 },
+      };
+      this.$http.post(url, req).then((res) => {
+        if (res?.data?.state === "SUCCESS" && res.data.data.length > 0) {
+          const config = res.data.data[0];
+          if (config?.default_srv_req_json) {
+            try {
+              this.config = {
+                ...config,
+                srv_req_no: config.default_srv_req_no,
+                filter_cols: config.filter_cols,
+
+              }
+              // this.config = JSON.parse(config.default_srv_req_json)
+              this.srvReqJson = JSON.parse(config.default_srv_req_json)
+
+            } catch (error) {
+
+            }
+          }
+
+          try {
+            this.srvReqJson = JSON.parse(this.config?.srv_req_json);
+          } catch (error) { }
+          if (this.srvReqJson?.serviceName) {
+            // this.page = this.srvReqJson.page
+            this.getListV2(this.srvReqJson?.serviceName).then(() => {
+              this.getList();
+              // this.getGroupFields()
+            });
+          }
+        }
+      });
+
     },
     getRequestCfg() {
       const url = "/config/select/srvpage_cfg_srv_call_select";
@@ -154,8 +259,7 @@ export default {
         condition: [
           { colName: "srv_call_no", ruleType: "like", value: this.requestNo },
         ],
-        page: { pageNo: 1, rownumber: 1 },
-        query_source: "list_page",
+        page: { pageNo: 1, rownumber: 1 }
       };
       this.$http.post(url, req).then((res) => {
         if (res?.data?.state === "SUCCESS" && res.data.data.length > 0) {
@@ -183,7 +287,7 @@ export default {
           {
             colName: "srv_req_no",
             ruleType: "eq",
-            value: this.requestNo,
+            value: this.config?.srv_req_no || this.requestNo,
           },
         ],
       };
@@ -191,16 +295,15 @@ export default {
       if (Array.isArray(res.data.data)) {
         let groupFields = res.data.data;
         let cols = res.data.data.reduce((res, cur) => {
-          if (!res.find(item => item.columns === cur.col_name)) {
-            let col = this.srvCols.find(e => e.columns === cur.col_name)
+          if (!res.find((item) => item.columns === cur.col_name)) {
+            let col = this.srvCols.find((e) => e.columns === cur.col_name);
             if (col) {
-              res.push(col)
+              res.push(col);
             }
           }
-          return res
-
-        }, [])
-        this.srvCols = cols
+          return res;
+        }, []);
+        this.srvCols = cols;
         const groupType = [
           "by",
           "by_year",
@@ -225,18 +328,22 @@ export default {
           "count_all",
           "distinct_count",
         ];
-        this.calcCols = groupFields.filter((item) => calcType.includes(item.type_stat)).map(item => {
-          return {
-            colName: item.col_name,
-            type: item.type_stat
-          }
-        });
-        this.groupCols = groupFields.filter((item) => item.type_stat === "by").map(item => {
-          return {
-            colName: item.col_name,
-            type: item.type_stat
-          }
-        })
+        this.calcCols = groupFields
+          .filter((item) => calcType.includes(item.type_stat))
+          .map((item) => {
+            return {
+              colName: item.col_name,
+              type: item.type_stat,
+            };
+          });
+        this.groupCols = groupFields
+          .filter((item) => item.type_stat === "by")
+          .map((item) => {
+            return {
+              colName: item.col_name,
+              type: item.type_stat,
+            };
+          });
         this.groupByCols = groupFields
           .filter((item) => groupType.includes(item.type_stat))
           .reduce((res, cur) => {
@@ -246,20 +353,20 @@ export default {
             if (fieldInfo) {
               cur.label = `按${fieldInfo.label}`;
               switch (cur.type_stat) {
-                case 'by_year':
-                  cur.label = '按年';
+                case "by_year":
+                  cur.label = "按年";
                   break;
-                case 'by_month':
+                case "by_month":
                 case "by_month_of_year":
-                  cur.label = '按月';
+                  cur.label = "按月";
                   break;
-                case 'by_week':
+                case "by_week":
                 case "by_week_of_year":
-                  cur.label = '按周';
+                  cur.label = "按周";
                   break;
-                case 'by_date':
+                case "by_date":
                 case "by_date_of_year":
-                  cur.label = '按日';
+                  cur.label = "按日";
                   break;
               }
               if (res[cur.col_name]?.list) {
@@ -268,15 +375,15 @@ export default {
                 res[cur.col_name] = {
                   list: [{ ...cur }],
                   key: cur.col_name,
-                  value: -1
+                  value: -1,
                 };
               }
             }
             return res;
           }, {});
-        this.groupByColsVal = {}
+        this.groupByColsVal = {};
         for (const key in this.groupByCols) {
-          this.groupByColsVal[key] = null
+          this.groupByColsVal[key] = null;
           // this.groupByColsVal[key] = this.groupByCols[key]
         }
       }
@@ -308,7 +415,6 @@ export default {
             Array.isArray(this.srvReqJson?.group) &&
             this.srvReqJson?.group.length > 0
           ) {
-
             this.srvCols = res.data?.data?.srv_cols.filter((item) => {
               let groupItem = this.srvReqJson?.group.find(
                 (e) => item.columns === e.colName
@@ -320,6 +426,17 @@ export default {
                 return true;
               }
             });
+            if (this.config.filter_cols) {
+              const filter_cols = this.config.filter_cols.split(',')
+              this.filterCols = this.srvCols.filter((item) => filter_cols.includes(item.columns))
+              if (this.filterCols.length > 0) {
+                this.filterCols.forEach(item => {
+                  this.$set(item, 'value', undefined)
+
+                  this.$set(this.filterModel, item.columns, undefined)
+                })
+              }
+            }
             await this.getGroupFields();
           }
         }
@@ -328,16 +445,30 @@ export default {
     async getList(p) {
       const url = `/${this.srvReqJson.mapp}/select/${this.srvReqJson.serviceName}`;
       const req = this.srvReqJson;
-      req.page = this.page || req.page
+      req.page = this.page || req.page;
       delete req.group;
       if (Array.isArray(this.curGroup) && this.curGroup.length > 0) {
-        req.group = this.curGroup
+        req.group = this.curGroup;
       } else {
         // req.group = [...this.groupCols, ...this.calcCols]
       }
-      this.onLoading = true
-      const res = await this.$http.post(url, req)
-      this.onLoading = false
+      if (Array.isArray(this.filterCols)) {
+        const condition = this.filterCols.map(item => {
+          if (item.value !== undefined) {
+            const obj = { colName: item.columns, ruleType: 'like', value: null }
+            if (item.col_type === 'Date') {
+              obj.value = item.value.toString()
+            } else {
+              obj.value = item.value
+            }
+            return obj
+          }
+        }).filter(item => item?.colName)
+        req.condition = [...req.condition, ...condition]
+      }
+      this.onLoading = true;
+      const res = await this.$http.post(url, req);
+      this.onLoading = false;
 
       if (res?.data?.state === "SUCCESS") {
         this.tableData = res.data.data;
@@ -347,7 +478,8 @@ export default {
   },
   mounted() {
     if (this.requestNo) {
-      this.getRequestCfg();
+      // this.getRequestCfg();
+      this.getListCfg()
     } else {
       this.$message.error("缺少requestNo参数");
     }
@@ -371,7 +503,8 @@ export default {
 }
 
 .group-box {
-  padding: 10px;
+  padding: 0 10px;
+  margin-bottom: 10px;
 
   .group-box-item {
     margin-right: 20px;
@@ -379,5 +512,11 @@ export default {
     padding: 10px;
     // border: 1px solid #eee;
   }
+}
+
+.el-input {}
+
+.el-date-editor.el-range-editor {
+  width: 100%;
 }
 </style>
