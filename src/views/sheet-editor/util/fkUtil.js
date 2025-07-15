3a337b80460e7f3e4dcf19d1822237d94be03776
jiaqi <2468260248@qq.com>
2025-07-15 17:20:24 +0800

    docs(sheet-editor): 为外键工具类添加JSDoc注释

--- src/views/sheet-editor/util/fkUtil.js
+++ src/views/sheet-editor/util/fkUtil.js
@@ -1,12 +1,26 @@
 import { isFk, isFkAutoComplete } from '@/utils/sheetUtils'
 import cloneDeep from 'lodash/cloneDeep'
 import { $http } from '@/common/http';
+
+/**
+ * 外键工具类，用于处理表格编辑器中的外键相关操作
+ * @class FkUtil
+ */
 export class FkUtil {
+  /**
+   * 构造函数
+   * @param {Object} column - 列配置对象
+   * @param {string} app - 应用名称
+   */
   constructor(column,app) {
     this.column = column;
     this.app = app;
   }
 
+  /**
+   * 获取编辑器类型
+   * @returns {string} 编辑器类型，可能的值：'autocomplete'、'fk' 或其他字段类型
+   */
   getEditorType() {
     if (isFkAutoComplete(this.column)) {
       return "autocomplete";
@@ -16,6 +30,11 @@ export class FkUtil {
     return getFieldType(this.column);
   }
 
+  /**
+   * 获取选项列表V3版本
+   * 根据列类型和操作类型返回相应的选项列表
+   * @returns {Array|undefined} 选项列表数组或undefined
+   */
   getOptionListV3() {
     const operateType = this.row?.__flag || "update";
     const editorType = this.getEditorType();
@@ -26,6 +45,11 @@ export class FkUtil {
     }
   }
 
+  /**
+   * 获取最终的选项列表
+   * 根据条件筛选出符合当前行数据的选项配置
+   * @returns {Object|null} 符合条件的选项配置对象或null
+   */
   getOptionListFinal() {
     let result = null;
     const option_list_v3 = this.getOptionListV3();
@@ -44,6 +68,13 @@ export class FkUtil {
     return result;
   }
 
+  /**
+   * 构建外键查询请求对象
+   * 根据输入值和规则类型构建用于查询外键数据的请求参数
+   * @param {string} inputVal - 输入值，用于查询匹配的数据
+   * @param {string} [ruleType="[like]"] - 查询规则类型，默认为模糊查询
+   * @returns {Object|undefined} 查询请求对象或undefined（当不是外键列时）
+   */
   getOptionReq(inputVal, ruleType = "[like]") {
     if (!isFk(this.column)) {
       return
@@ -114,6 +145,14 @@ export class FkUtil {
     }
     return req;
   }
+  
+  /**
+   * 异步获取匹配的值
+   * 根据查询字符串从服务器获取匹配的外键数据
+   * @param {string} queryString - 查询字符串
+   * @param {string} [ruleType="[like]"] - 查询规则类型，默认为模糊查询
+   * @returns {Promise<Object|undefined>} 返回匹配的数据对象，包含label和value属性，如果没有匹配则返回undefined
+   */
   async getMatchedValue(queryString, ruleType = "[like]") {
     const optionListFinal = this.getOptionListFinal();
     let req = cloneDeep(this.getOptionReq(queryString, ruleType));
