07564cb7fc1ad0b717cf428427e9d1c6be2ccf8a
jiaqi <2468260248@qq.com>
2025-07-18 15:48:27 +0800

    refactor(header-cell): 将组件从选项式API重构为组合式API
    - 使用setup语法糖重构组件逻辑
    - 将方法改为使用箭头函数
    - 将计算属性改为使用computed
    - 优化模板代码格式和可读性
    - 重命名事件处理函数以保持一致性

--- src/views/sheet-editor/components/header-cell.vue
+++ src/views/sheet-editor/components/header-cell.vue
@@ -3,13 +3,20 @@
     <div class="flex items-center justify-between w-full flex-row-reverse truncate " v-if="!childListType">
       <div class="flex items-center header-util-box">
         <div class="icon filter-icon cursor-pointer">
-          <header-filter :column="column" :condition="condition" :app="app" :list="list" :service="service"
-            @filter-change="filterChange"></header-filter>
+          <header-filter 
+            :column="column" 
+            :condition="condition" 
+            :app="app" 
+            :list="list" 
+            :service="service"
+            @filter-change="handleFilterChange"
+          />
         </div>
-        <div class="icon sort-icon" @click="onSrotChange" v-if="!['MultilineText', 'File', 'Image', 'RichText','snote'].includes(
-          column.col_type
-        )
-          ">
+        <div 
+          class="icon sort-icon" 
+          @click="handleSortChange" 
+          v-if="!nonSortableTypes.includes(column.col_type)"
+        >
           <i class="el-icon-caret-top cursor-pointer" :class="{ active: curSort === 'ASC' }"></i>
           <i class="el-icon-caret-bottom" :class="{ active: curSort === 'DESC' }"></i>
         </div>
@@ -21,22 +28,22 @@
         <el-tooltip class="item" effect="dark" content="可编辑列" placement="bottom-end" v-if="column.editable">
           <i class="el-icon-edit-outline"></i>
         </el-tooltip>
-        <el-tooltip class="item" effect="dark" content="autocomplete" placement="bottom-end" v-if="isFkAutoComplete(column)">
+        <el-tooltip class="item" effect="dark" content="autocomplete" placement="bottom-end" v-if="isAutoCompleteField">
           <i class="el-icon-edit"></i>
         </el-tooltip>
-        <el-tooltip class="item" effect="dark" content="fk" placement="bottom-end" v-if="isFk(column)">
+        <el-tooltip class="item" effect="dark" content="fk" placement="bottom-end" v-if="isForeignKeyField">
           <i class="el-icon-search"></i>
         </el-tooltip>
       </div>
-
     </div>
-    <div class="flex items-center w-full"
-    :class="{
-      'justify-center':!childListType,
-      'justify-start p-x-2':!!childListType
-    }"
+    <div 
+      class="flex items-center w-full"
+      :class="{
+        'justify-center': !childListType,
+        'justify-start p-x-2': !!childListType
+      }"
     >
-      <el-tooltip class="item" effect="dark" content="必填" placement="bottom-center" v-if="childListType&&column.isRequired">
+      <el-tooltip class="item" effect="dark" content="必填" placement="bottom-center" v-if="childListType && column.isRequired">
         <span class="required color-red m-r-2 font-bold">*</span>
       </el-tooltip>
       <el-tooltip effect="dark" :content="column.label">
@@ -46,70 +53,103 @@
   </div>
 </template>
 
-<script>
-import headerFilter from "./header-filter.vue";
-import { isFkAutoComplete ,isFk} from '@/utils/sheetUtils.js'
+<script setup>
+import { ref, computed } from 'vue'
+import headerFilter from "./header-filter.vue"
+import { isFkAutoComplete, isFk } from '@/utils/sheetUtils.js'
 
-export default {
-  components: {
-    headerFilter,
+// Props 定义
+const props = defineProps({
+  column: {
+    type: Object,
+    required: true,
+    default: () => ({})
   },
-  props: {
-    column: Object,
-    sortState: Object,
-    app: String,
-    service: String,
-    list: Array,
-    condition: Array,
-    childListType:String
+  sortState: {
+    type: Object,
+    default: () => ({})
   },
-  data() {
-    return {
-      checkedOption: [],
-      oldCheckedOption: [],
-      onFilter: false,
-      filterVisible: false,
-      isFkAutoComplete,
-      isFk
-    };
+  app: {
+    type: String,
+    default: ''
   },
-  methods: {
-    filterChange(val) {
-      console.log(val);
-      this.$emit("filter-change", val);
-    },
-    toFilter() {
-      this.filterVisible = false;
-      this.$emit("filter-change", {
-        colName: this.column.columns,
-        ruleType: "in",
-        value: this.checkedOption.toString(),
-        remove: !this.checkedOption?.length,
-      });
-      this.onFilter = !!this.checkedOption?.length;
-    },
-    handleCheckedChange(e) {
-      console.log(e);
-    },
-    onSrotChange() {
-      this.$emit("sort-change", this.column.columns);
-    },
+  service: {
+    type: String,
+    default: ''
   },
-  computed: {
-    optionList() {
-      return this.column.col_type === "Enum" &&
-        this.column?.option_list_v2?.length
-        ? this.column?.option_list_v2
-        : null;
-    },
-    curSort() {
-      if (this.column?.columns) {
-        // return this.sortState?.get(this.column.columns);
-        return this.sortState?.[this.column.columns];
-      }
-    },
+  list: {
+    type: Array,
+    default: () => []
   },
-};
+  condition: {
+    type: Array,
+    default: () => []
+  },
+  childListType: {
+    type: String,
+    default: ''
+  }
+})
+
+// Emits 定义
+const emit = defineEmits(['filter-change', 'sort-change'])
+
+// 响应式数据
+const checkedOption = ref([])
+const oldCheckedOption = ref([])
+const onFilter = ref(false)
+const filterVisible = ref(false)
+
+// 常量定义
+const nonSortableTypes = ['MultilineText', 'File', 'Image', 'RichText', 'snote']
+
+// 计算属性
+const optionList = computed(() => {
+  return props.column.col_type === "Enum" &&
+    props.column?.option_list_v2?.length
+    ? props.column?.option_list_v2
+    : null
+})
+
+const curSort = computed(() => {
+  if (props.column?.columns) {
+    return props.sortState?.[props.column.columns]
+  }
+  return undefined
+})
+
+const isAutoCompleteField = computed(() => {
+  return isFkAutoComplete(props.column)
+})
+
+const isForeignKeyField = computed(() => {
+  return isFk(props.column)
+})
+
+// 方法定义
+const handleFilterChange = (val) => {
+  console.log('Filter change:', val)
+  emit("filter-change", val)
+}
+
+const handleSortChange = () => {
+  emit("sort-change", props.column.columns)
+}
+
+const toFilter = () => {
+  filterVisible.value = false
+  emit("filter-change", {
+    colName: props.column.columns,
+    ruleType: "in",
+    value: checkedOption.value.toString(),
+    remove: !checkedOption.value?.length,
+  })
+  onFilter.value = !!checkedOption.value?.length
+}
+
+const handleCheckedChange = (e) => {
+  console.log('Checked change:', e)
+}
 </script>
 
 <style lang="scss" scoped>
